6b03a1ae80d39767ca55dc3db8cab07d320f3f1a
Eric Laursen <ericjlaursen@gmail.com>
2018-11-15 15:21:21 -0500

    Connects client to server socket

--- components/app.js
+++ components/app.js
@@ -4,6 +4,25 @@ import '../public/index.css'
 import io from 'socket.io-client'
 
 class App extends React.Component{
+	constructor(props){
+		super(props)
+		this.socket;
+		this.connectSocket = this.connectSocket.bind(this)	
+	}
+	
+	connectSocket(){
+		console.log('client connected to socket it')
+		console.log(this.socket.id)
+	}
+
+	componentWillMount(){
+		//connect socket server
+		this.socket = io('http://localhost:3000')
+
+		//add a listener to the socket for the connect event
+		this.socket.on('connect', this.connectSocket);
+	}
+
 	render(){
 		return (
 			<div>
