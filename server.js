053fc8e3068876321be59abcae15c6fe40e569c7
Eric Laursen <ericjlaursen@gmail.com>
2018-11-15 16:07:50 -0500

    terminal logs number of sockets on connect & disconnect.

--- server.js
+++ server.js
@@ -11,10 +11,29 @@ var httpServer = app.listen(3000)
 //create a SOCKET server
 var io = require('socket.io').listen(httpServer)
 
+
+let connections = [];
+
 //connection happens when a socket gets connected
 io.sockets.on('connection', (connectedSocket) => {
-	console.log('New connectedSocket')
+
+	//when socket DISconnects
+	//once happens ONCE, not like an 'on'
+	connectedSocket.once('disconnect', () => {
+		//remove socket from connections arr
+		connections.splice(connections.indexOf(connectedSocket), 1);
+		connectedSocket.disconnect();
+		console.log('socket disconnected, remaining sockets')
+		console.log(connections.length)
+	})
+
+
+	//add current socket to connections array
+	connections.push(connectedSocket)
+	console.log('socket CONNECTED, connected sockets:')
+	console.log(connections.length)
 	console.log(connectedSocket.id)
+
 })
 
 console.log('server running!')
