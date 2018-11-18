9387e26073f35c857e78b8b5a22e004f436c6e4c
Eric Laursen <ericjlaursen@gmail.com>
2018-11-18 16:32:36 -0500

    Single-input form with submit fn

---
+++ components/JoinForm/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css'
+
+class JoinForm extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			fullName: ''
+		}
+
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.updateFormVal = this.updateFormVal.bind(this)
+	}
+
+	updateFormVal(e){
+		let idVal = e.target.id
+
+		if (idVal === 'fullName'){
+			this.setState({fullName: e.target.value})
+		}
+	}
+	
+	handleSubmit(e){
+		e.preventDefault();
+		console.log('submitted Fn here!')
+		console.log(this.state.fullName)
+	}
+
+	render(){
+		// console.log('Join Form Props')
+		// console.log(this.props)
+		return (
+			<form className="joinForm" onSubmit={this.handleSubmit} >
+				<label for="fullName" className='inputlabel'>Full Name
+					<input 
+						id="fullName"
+						className='input fullName'
+						placeholder="enter your full name..."
+						value={this.state.fullName}
+                    	onChange={this.updateFormVal}
+						required/>
+					<input type="submit" value="Join" />
+				</label>
+			</form>
+		);
+	}
+}
+  
+export default JoinForm;
