72d2f9b9aed0e8cbe8ff3cb72334cee1f1402da5
Eric Laursen <ericjlaursen@gmail.com>
2018-11-18 16:32:44 -0500

    Connects JoinForm

--- components/Audience/index.js
+++ components/Audience/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import './index.css'
 import PollContent from '../PollContent'
+import JoinForm from '../JoinForm'
 
 let Audience = (props) => {
 	console.log('audience props')
 	console.log(props)
 	return (
-		<PollContent showContent={props.connectedStatus}>
-			<h1>Showing PollContent child</h1>
-		</PollContent>
+		<React.Fragment>
+			<h1>{props.title}</h1>
+			<PollContent showContent={props.connectedStatus}>
+				<JoinForm />
+			</PollContent>
+		</React.Fragment>
 	);
 }
   
