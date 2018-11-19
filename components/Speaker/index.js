c41498943b9f41f275724853c1a6be76537c4ebe
Eric Laursen <ericjlaursen@gmail.com>
2018-11-19 12:27:20 -0500

    conditionally renders speakerStartForm

--- components/Speaker/index.js
+++ components/Speaker/index.js
@@ -1,9 +1,31 @@
 import React from "react";
 import './index.css'
+import SpeakerStartForm from '../SpeakerStartForm'
+import Header from '../Header'
+import ShowThis from '../ShowThis'
 
 let Speaker = (props) => {
+	console.log("speaker props")
+	console.log(props)
 	return (
-		<h1>Speaker</h1>
+		
+		<React.Fragment>
+			<Header title={props.title} connectedStatus={props.connectedStatus}/>
+			<ShowThis showContent={(props.connectedStatus == true)}>
+
+				<ShowThis showContent={(props.memberStats && props.memberStats.type == 'speaker' && props.memberStats.memberName)}>
+					<p>Questions</p>
+					<p>Attendance</p>
+				</ShowThis>
+
+				<ShowThis showContent={(!props.memberStats)}>
+					<p>Start the presentaiton</p>
+					<SpeakerStartForm emit={props.emit} />
+				</ShowThis>
+
+			</ShowThis>
+		</React.Fragment>
+
 	);
 }
   
