045ae9458df58fb804660eb2222903cad96d4d54
Eric Laursen <ericjlaursen@gmail.com>
2018-11-20 07:04:11 -0500

    Adds a table to the component

--- components/AttendenceNote/index.js
+++ components/AttendenceNote/index.js
@@ -2,8 +2,29 @@ import React from "react";
 import './index.css'
 
 let AttendanceNote = (props) => {
+	let attendanceRows = props.audience.map(mem => {
+		return (
+			<tr key={mem.id}>
+				<td>{mem.fullName}</td>
+				<td>{mem.id}</td>
+			</tr>
+			)
+	})
 	return (
-		<h1>Attendance Note</h1>
+		<React.Fragment>
+			<h1>Attendees: {props.audience.length}</h1>
+			<table>
+				<thead>
+					<tr>
+						<th>Member</th>
+						<th>SocketID</th>
+					</tr>
+				</thead>
+				<tbody>
+					{attendanceRows}
+				</tbody>
+			</table>
+		</React.Fragment>	
 	);
 }
   
