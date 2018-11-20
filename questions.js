a1150e7b2001275521f24c26543e8f989c96c2d1
Eric Laursen <ericjlaursen@gmail.com>
2018-11-20 16:10:30 -0500

    placeholder questions

---
+++ questions.js
@@ -0,0 +1,23 @@
+module.exports = [
+	{
+		"q": "Who is the greatest basketball player of all time?",
+		"a": "Jordan",
+		"b": "Lebron",
+		"c": "D.Rodman",
+		"d": "Javale McGee"
+	},
+	{
+		"q": "Where is the best food from in the world?",
+		"a": "Southeast Asia",
+		"b": "Italy",
+		"c": "South America",
+		"d": "The Fridge"
+	},
+	{
+		"q": "How do Great minds think?",
+		"a": "Alike",
+		"b": "For themselves",
+		"c": "With Their Brain",
+		"d": "Hard"
+	}
+]
