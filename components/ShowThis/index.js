5bb7ccfe06b477cc51693c3e4d699fdf4e968e95
Eric Laursen <ericjlaursen@gmail.com>
2018-11-23 21:34:49 -0500

    updates code layout

--- components/ShowThis/index.js
+++ components/ShowThis/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import './index.css'
 
-let ShowThis = (props) => {
+export default function ShowThis(props){
 	// console.log('ShowThis props')
 	// console.log(props)
 	return (props.showContent ? <div className="showThisWrapper">{props.children}</div> : null);
-}
-  
-export default ShowThis; 
+} 
